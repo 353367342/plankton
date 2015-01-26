a1c0fb1f3ff11dc0aed7b9a249be01cf094760a7
Chris Curro <curro@cooper.edu>
2015-01-26 18:53:34 -0500

    expanded net size in cuda examples

--- examples/min_cuda_float.lua
+++ examples/min_cuda_float.lua
@@ -5,11 +5,11 @@ require 'cutorch'
 output = torch.CudaTensor(58):fill(1)
 
 mdl = nn.Sequential()
-mdl:add(nn.SpatialConvolutionMM(1,10,10,10))
+mdl:add(nn.SpatialConvolutionMM(1,10,7,7))
 mdl:add(nn.SpatialMaxPooling(2,2))
 mdl:add(nn.SpatialConvolutionMM(10,1,10,10))
-mdl:add(nn.Reshape(36*36))
-mdl:add(nn.Linear(36*36,2))
+mdl:add(nn.Reshape(244*244))
+mdl:add(nn.Linear(244*244,2))
 mdl:add(nn.LogSoftMax())
 mdl:cuda()
 
@@ -18,7 +18,7 @@ criterion:cuda()
 
 for i=1,512 do
   local currentError = 0
-  input = torch.randn(58,1,100,100):cuda()
+  input = torch.randn(58,1,512,512):cuda()
   oHat = mdl:forward(input)
   currentError = currentError + criterion:forward(oHat,output)
   mdl:zeroGradParameters()
