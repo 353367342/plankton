fccbe276d79bb5a54a3c2b590de2f7a160e74dd1
Chris Curro <curro@cooper.edu>
2015-01-25 21:51:41 -0500

    min work ex of cuda with float default

---
+++ examples/min_cuda_float.lua
@@ -0,0 +1,29 @@
+require 'nn'
+require 'cunn'
+require 'cutorch'
+
+output = torch.CudaTensor(58):fill(1)
+
+mdl = nn.Sequential()
+mdl:add(nn.SpatialConvolutionMM(1,10,10,10))
+mdl:add(nn.SpatialMaxPooling(2,2))
+mdl:add(nn.SpatialConvolutionMM(10,1,10,10))
+mdl:add(nn.Reshape(36*36))
+mdl:add(nn.Linear(36*36,2))
+mdl:add(nn.LogSoftMax())
+mdl:cuda()
+
+criterion = nn.ClassNLLCriterion()
+criterion:cuda()
+
+for i=1,512 do
+  input = torch.randn(58,1,100,100):cuda()
+  oHat = mdl:forward(input)
+  criterion:forward(oHat,output)
+  mdl:zeroGradParameters()
+  mdl:backward(input,criterion:backward(mdl.output,output))
+  mdl:updateParameters(1e-3)
+  collectgarbage()
+end
+
+print(torch.pow(10,oHat:float()))
