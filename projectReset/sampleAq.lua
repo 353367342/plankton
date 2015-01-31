8e009a759ab2542a9d12e2829f9451e9e0a9b434
Chris Curro <curro@cooper.edu>
2015-01-31 14:20:37 -0500

    updated model, .76 cv at 106 epochs

--- projectReset/sampleAq.lua
+++ projectReset/sampleAq.lua
@@ -7,12 +7,12 @@ function getTrainSample()
       local augExample   = randomTransform(rawExample[1],augSize)
       batch[{{1 + (j-1)*augSize,j*augSize},{1,1},{1,sampleSize[2]},{1,sampleSize[3]}}] = augExample
       targets:narrow(1,1 +(j-1)*augSize,augSize):fill(dataset[splitInd[sampleList[j]]].classNum)
-      end
-   return batch, targets
    end
+   return batch, targets
+end
 
 function getCrValSample(n)
---      local sampleList = torch.randperm(#dataset - trainEnd -1):narrow(1,1,valBatchSize):add(valBegin)
+   --      local sampleList = torch.randperm(#dataset - trainEnd -1):narrow(1,1,valBatchSize):add(valBegin)
    local sampleList = splitInd[{{valBegin,#dataset}}]:narrow(1,1 + (n-1)*valBatchSize,valBatchSize)
    local batch = torch.CudaTensor(valBatchSize,sampleSize[1],sampleSize[2],sampleSize[3])
    local targets = torch.CudaTensor(valBatchSize)
@@ -21,6 +21,17 @@ function getCrValSample(n)
       rawExample[{{1,1},{1,1},{1,sampleSize[2]},{1,sampleSize[3]}}] = image.load(dataset[splitInd[sampleList[j]]].relPath)
       batch[{{1 + (j-1),j},{1,1},{1,sampleSize[2]},{1,sampleSize[3]}}] = rawExample[1]:resize(1,sampleSize[1],sampleSize[2],sampleSize[3])
       targets:narrow(1,1 +(j-1),1):fill(dataset[splitInd[sampleList[j]]].classNum)
-      end
+   end
    return batch, targets
+end
+
+function getTestSample(n)
+   local sampleList = testSet[{{valBegin,#dataset}}]:narrow(1,1 + (n-1)*valBatchSize,valBatchSize)
+   local batch = torch.CudaTensor(valBatchSize,sampleSize[1],sampleSize[2],sampleSize[3])
+   for j=1,valBatchSize do
+      local testExample = torch.CudaTensor(1,3,sampleSize[2],sampleSize[3])
+      testExample[{{1,1},{1,1},{1,sampleSize[2]},{1,sampleSize[3]}}] = image.load(dataset[splitInd[sampleList[j]]].relPath)
+      batch[{{1 + (j-1),j},{1,1},{1,sampleSize[2]},{1,sampleSize[3]}}] = rawExample[1]:resize(1,sampleSize[1],sampleSize[2],sampleSize[3])
+   end
+   return batch
    end
