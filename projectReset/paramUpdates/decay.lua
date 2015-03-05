dd4e95d0de7d71e5a7d8479ae8ef133101c4209a
Chris Curro <curro@cooper.edu>
2015-03-05 11:26:25 -0500

    param updates for dual teslas

--- projectReset/paramUpdates/decay.lua
+++ projectReset/paramUpdates/decay.lua
@@ -1,10 +1,16 @@
-function setDecay()
+function setDecay(a)
 	local decay = optimState.weightDecay
-	if file_exists('decay') then
-		local f = io.open('decay','r')
+	local fName = {}
+	if a == 1 then
+		fName = 'rate1'
+	elseif a == 2 then
+		fName = 'rate2'
+	end
+	if file_exists(fName) then
+		local f = io.open(fName,'r')
 		decay = tonumber(f:read('*line'))
 		f:close()
-		os.remove('decay')
+		os.remove(fName)
 	end
 	return decay
 end
--- projectReset/paramUpdates/rate.lua
+++ projectReset/paramUpdates/rate.lua
@@ -1,10 +1,16 @@
-function setRate()
+function setRate(a)
 	local rate = optimState.learningRate
-	if file_exists('rate') then
-		local f = io.open('rate','r')
+	local fName = {}
+	if a == 1 then
+		fName = 'rate1'
+	elseif a == 2 then
+		fName = 'rate2'
+	end
+	if file_exists(fName) then
+		local f = io.open(fName,'r')
 		rate = tonumber(f:read('*line'))
 		f:close()
-		os.remove('rate')
+		os.remove('fName')
 	end
 	return rate
 end
